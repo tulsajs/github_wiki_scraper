ff10dd6068c50d1d94b6580a66f3e73b4d4bf3b4
nearbycoder <nearbycoder@gmail.com>
2016-04-24 10:38:47 -0500

    Turned callback into Promise

--- example.js
+++ example.js
@@ -2,6 +2,6 @@ scraper = require('./index.js');
 
 var github = {user: 'TulsaJS', repo: 'tulsajs.com', month: 'May', year: 2016}
 
-scraper.getEventsInfo(github, function(results) {
+scraper.getEventsInfo(github).then(function(results) {
   console.log(results)
 })
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var Promise = require('promise');
 
 //function to scrape github wiki pages for TulsaJS Events.
-function getEventsInfo(github, callback) {
-  request(`https://github.com/${github.user}/${github.repo}/wiki/${github.month}-${github.year}-Meeting`, function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      if($('.gh-header-title').html() != 'Home') {
-        var wikiPage = {};
-        wikiPage.title = $('.gh-header-title').html();
-        wikiPage.when = $('p > strong:contains("When")').next().html();
-        wikiPage.time = $('p > strong:contains("Time")').next().html();
-        wikiPage.whereName = $('p > strong:contains("Where-Name")').next().html();
-        wikiPage.wherePlace = $('p > strong:contains("Where-Place")').next().html();
-        wikiPage.topics = $('p > strong:contains("Topics")').parent().next().html();
-        wikiPage.success = true;
-      } else {
-        var wikiPage = {error: 'Event Not Planned!'}
+function getEventsInfo(github) {
+  return new Promise(function (resolve, reject) {
+    request(`https://github.com/${github.user}/${github.repo}/wiki/${github.month}-${github.year}-Meeting`, function (error, response, html) {
+      if (error) reject(error);
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        if($('.gh-header-title').html() != 'Home') {
+          var wikiPage = {};
+          wikiPage.title = $('.gh-header-title').html();
+          wikiPage.when = $('p > strong:contains("When")').next().html();
+          wikiPage.time = $('p > strong:contains("Time")').next().html();
+          wikiPage.whereName = $('p > strong:contains("Where-Name")').next().html();
+          wikiPage.wherePlace = $('p > strong:contains("Where-Place")').next().html();
+          wikiPage.topics = $('p > strong:contains("Topics")').parent().next().html();
+          wikiPage.success = true;
+        } else {
+          var wikiPage = {error: 'Event Not Planned!'}
+        }
+        resolve(wikiPage);
       }
-      return callback(wikiPage)
-    }
+    });
   });
 }
 
