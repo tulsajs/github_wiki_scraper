0be2f588fa2855467a88562b2073d566ae90f988
nearbycoder <nearbycoder@gmail.com>
2016-05-02 08:01:32 -0500

    Added meetup link to api call

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function getEventsInfo(github) {
           wikiPage.title = $('.gh-header-title').html();
           wikiPage.when = $('p > strong:contains("When")').next().html();
           wikiPage.time = $('p > strong:contains("Time")').next().html();
+          wikiPage.meetup = $('p > strong:contains("Meetup")').next().html();
           wikiPage.whereName = $('p > strong:contains("Where-Name")').next().html();
           wikiPage.wherePlace = $('p > strong:contains("Where-Place")').next().html();
           $('p > strong:contains("Topics")').parent().next().children('li').each(function(li){
